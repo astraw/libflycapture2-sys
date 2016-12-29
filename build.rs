4f8c9c6e9969a1d5c45377aa182482e68daa0828
Andrew Straw <strawman@astraw.com>
2016-12-29 12:14:46 +0100

    builds on Windows (tested on x86_64, not on i686)

--- build.rs
+++ build.rs
@@ -2,17 +2,25 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let flycap_root = match env::var("FLYCAP_ROOT") {
+    let target = env::var("TARGET").expect("getting target");
+
+    let libdir = match env::var("FLYCAP_LIBDIR") {
         Ok(dir) => dir,
-        Err(e) => {
-            panic!("Environment variable FLYCAP_ROOT could not be read: {}. (Tip: set to /usr)", e);
+        Err(_) => {
+            match target.as_ref() {
+                "x86_64-pc-windows-msvc" | "x86_64-pc-windows-gnu" => {r"C:\Program Files\Point Grey Research\FlyCapture2\lib64\C".into()},
+                "i686-pc-windows-msvc" | "i686-pc-windows-gnu" => {r"C:\Program Files\Point Grey Research\FlyCapture2\lib32\C".into()},
+                _ => {"/usr/lib".into()},
+            }
         }
     };
 
-    let flycap_path = Path::new(&flycap_root);
-    let libdir = flycap_path.join("lib");
+    let libname = match target.as_ref() {
+        "x86_64-pc-windows-msvc" | "i686-pc-windows-msvc" | "x86_64-pc-windows-gnu" | "i686-pc-windows-gnu" => "FlyCapture2_C",
+        _ => "flycapture-c",
+    };
 
-    let libname = "flycapture-c";
+    let libdir = Path::new(&libdir);
 
     println!("cargo:rustc-link-search=native={}",libdir.display());
     // println!("cargo:rustc-link-lib=static={}", libname); // PGR doesn't ship static libs?
